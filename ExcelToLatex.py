1c9c5f8e34004fd3907edbb93923d8f7e93af86f
ITK13201 <57588603+ITK13201@users.noreply.github.com>
2020-02-20 20:16:01 +0900

    feat0220

--- ExcelToLatex.py
+++ ExcelToLatex.py
@@ -1,61 +1,89 @@
-import openpyxl, pyperclip
+import openpyxl, pyperclip, os
 from openpyxl.utils import get_column_letter
 
-# make
-'''
-\begin{table}[h]
-			\caption{ageの統計量と代表値}
-			\centering
-			\begin{tabular}{cr}
-				\hline
-				統計量または代表値	&	値\\
-				\hline \hline
-				中央値	&	43.00\\
-				最頻値	&	50.00\\
-				最大値	&	60.00\\
-				最小値	&	20.00\\
-				範囲		&	40.00\\
-				分散		&	122.35\\
-				標準偏差	&	11.06\\
-				\hline
-			\end{tabular}
-		\end{table}
-'''
-
 # ExcelWorksheetを読み込む
-#wbname = input('Excel-Workbook-Name: ')
-wbname = 'data.xlsx'
+wbname = input('Excel-Workbook-Name: ')
+wbsheet = input('Excel-Workbook-Sheet-Name: ')
+
 wb = openpyxl.load_workbook(wbname, data_only=True)
-#wbsheet = input('Excel-Workbook-Sheet-Name: ')
-wbsheet = 'toLatex'
 sheet = wb[wbsheet]
 sheet = wb.active
 
-linecode = 'ccccc'
-OutputText_begin =  '\\begin{tabular}{' + linecode + '} \n' \
-                    '\t\\hline'
+caption = input('caption: ')
+while True:
+	linecode = input('linecode: ')
+	if len(linecode) == sheet.max_column:
+		break
+	print('wrong code. input again.')
+
+decimal_places = int(input('decimal places: '))
+
+OutputText_begintable =  '\\begin{table}[h]\n'
+OutputText_begintabular = 	'\t\\centering\n' \
+							'\t\\begin{tabular}{' + linecode + '} \n' \
+                    		'\t\t\\hline\n'
     
 
-OutputText_end =    '\tend{tabular}\n' \
-                    '\\end\{table}'
+OutputText_end =    '\t\t\\hline\n' \
+					'\t\\end{tabular}\n' \
+                    '\\end{table}\n'
 
+caption_text = '\t\\caption{' + caption + '}\n'
 
-# debug #####################
+# クリップ用ファイルへの書き込み
+clip_file = open('{}.txt'.format(caption), 'w')
+
+clip_file.write(OutputText_begintable)
+clip_file.write(caption_text)
+clip_file.write(OutputText_begintabular)
+
 max_cell = get_column_letter(sheet.max_column) + str(sheet.max_row)
 
 for row_of_cell_obj in sheet['B2':max_cell]:
     for cell_obj in row_of_cell_obj:
-        cell_obj.value = round(cell_obj.value, 1)
+        cell_obj.value = round(cell_obj.value, decimal_places)
 
 for row_of_cell_obj in sheet['A1':max_cell]:
-    for cell_obj in row_of_cell_obj:
-        print(cell_obj.value, end='\t')
-    print()
+	clip_file.write('\t\t')
+	for cell_obj in row_of_cell_obj:
+		if cell_obj.column == sheet.max_column:
+			clip_file.write(str(cell_obj.value) + '\t\\\\')
+		else:
+			clip_file.write(str(cell_obj.value) + '\t&\t')
+	clip_file.write('\n')
 
-print(OutputText_begin)
-print(OutputText_end)
+clip_file.write(OutputText_end)
 
+print()
 
+# 結果をターミナル上に表示
+print('-----------BEGIN OF TEXT-----------')
 
+print(OutputText_begintable, end='')
+print(caption_text, end='')
+print(OutputText_begintabular, end='')
 
-##############################
+for row_of_cell_obj in sheet['A1':max_cell]:
+	print('\t\t', end='')
+	for cell_obj in row_of_cell_obj:
+		if cell_obj.column == sheet.max_column:
+			print(cell_obj.value, end='\t\\\\')
+		else:
+			print(cell_obj.value, end='\t&\t')
+	print()
+
+print(OutputText_end, end='')
+
+print('------------END OF TEXT------------')
+
+clip_file.close()
+
+# クリップ用テキストの中身を読み込む
+clip_file = open('{}.txt'.format(caption), 'r')
+copy_clip = clip_file.read()
+clip_file.close()
+
+# クリップボードにコピー
+pyperclip.copy(copy_clip)
+
+os.remove(caption + '.txt')
